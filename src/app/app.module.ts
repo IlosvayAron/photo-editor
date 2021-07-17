58fe959f44c335ab8c419091c987bed5837cd343
ilosvayaron <ilosvayaron@gmail.com>
2021-07-17 21:53:57 +0200

    One component & some modules was imported

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,39 @@
+// import { PhotoEditorComponent } from './photo-editor/photo-editor.component';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ImageCroppperComponent } from './image-croppper/image-croppper.component';
+// import { ImageCroppperComponent } from './image-croppper/image-croppper.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgxDropzoneModule } from 'ngx-dropzone';
+import { base64ToFile, ImageCropperModule } from 'ngx-image-cropper';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import {MatSliderModule} from '@angular/material/slider';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ImageCroppperComponent
+    // ImageCroppperComponent,
+    // PhotoEditorComponent
+    routingComponents
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    NgxDropzoneModule
+    NgxDropzoneModule,
+    ImageCropperModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    MatSliderModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]

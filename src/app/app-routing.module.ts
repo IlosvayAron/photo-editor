f70f2b12f2485c34c7c3014468a75430844db32d
ilosvayaron <ilosvayaron@gmail.com>
2021-07-17 21:49:06 +0200

    Created routing between websites

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ImageCroppperComponent } from './image-croppper/image-croppper.component';
+import { PhotoEditorComponent } from './photo-editor/photo-editor.component'
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: ImageCroppperComponent},
+  {path: 'first-picture', component: PhotoEditorComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
+export const routingComponents = [ImageCroppperComponent, PhotoEditorComponent]

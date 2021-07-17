f1ce2ef2971e5decea605dd4f95e70c0e0903f73
ilosvayaron <ilosvayaron@gmail.com>
2021-07-17 22:06:04 +0200

    Updated ImageCroppperComponent using the service

--- src/app/image-croppper/image-croppper.component.ts
+++ src/app/image-croppper/image-croppper.component.ts
@@ -1,30 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { ImageService } from "../services/image.service";
 
 @Component({
   selector: 'app-image-croppper',
   templateUrl: './image-croppper.component.html',
-  styleUrls: ['./image-croppper.component.scss']
+  styleUrls: ['./image-croppper.component.scss'],
+  providers: [ImageService]
 })
 export class ImageCroppperComponent implements OnInit {
 
-  constructor() {
-    
-   }
+  constructor(private imageService: ImageService) { }
 
   ngOnInit(): void {
   }
 
   files: File[] = [];
 
-  onSelect(event: { addedFiles: any; }){
-    console.log(event);
-    this.files.push(...event.addedFiles);
-  }
-
-  onRemove(event: File) {
-    console.log(event);
-    this.files.splice(this.files.indexOf(event), 1);
-  }
-
+  onSelect(event: {addedFiles: any;}){
+    return this.imageService.onSelect(event);
+  };
 
 }

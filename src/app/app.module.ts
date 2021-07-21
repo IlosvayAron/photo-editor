fa8559548ed881c80f28a485a01d725520631e23
ilosvayaron <ilosvayaron@gmail.com>
2021-07-21 16:36:02 +0200

    Update app-module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,8 @@
-// import { PhotoEditorComponent } from './photo-editor/photo-editor.component';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
-// import { ImageCroppperComponent } from './image-croppper/image-croppper.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgxDropzoneModule } from 'ngx-dropzone';
 import { base64ToFile, ImageCropperModule } from 'ngx-image-cropper';
@@ -18,9 +16,7 @@ import { FormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
     AppComponent,
-    // ImageCroppperComponent,
-    // PhotoEditorComponent
-    routingComponents
+    routingComponents,
   ],
   imports: [
     BrowserModule,

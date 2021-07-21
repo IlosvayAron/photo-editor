8cda517d4ee947700ef75d1174f33a7850e5d47c
ilosvayaron <ilosvayaron@gmail.com>
2021-07-21 16:34:07 +0200

    New sites added to the routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ImageCroppperComponent } from './image-croppper/image-croppper.component';
 import { PhotoEditorComponent } from './photo-editor/photo-editor.component'
+import { SecondPictureComponent } from './second-picture/second-picture.component';
+import { SeoComponent } from './seo/seo.component';
 
 const routes: Routes = [
   {path: '', component: ImageCroppperComponent},
   {path: 'first-picture', component: PhotoEditorComponent},
+  {path: 'second-picture', component: SecondPictureComponent},
+  {path: 'seo', component: SeoComponent},
 ];
 
 @NgModule({
@@ -14,4 +18,4 @@ const routes: Routes = [
 })
 export class AppRoutingModule { }
 
-export const routingComponents = [ImageCroppperComponent, PhotoEditorComponent]
+export const routingComponents = [ImageCroppperComponent, PhotoEditorComponent, SecondPictureComponent, SeoComponent]

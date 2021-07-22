b1a93a75bd3e1e6235bf3d363c148a8d0298078b
ilosvayaron <ilosvayaron@gmail.com>
2021-07-22 16:21:10 +0200

    New style for webpages

--- src/app/seo/seo.component.ts
+++ src/app/seo/seo.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Editor, Toolbar } from 'ngx-editor';
 
 @Component({
   selector: 'app-seo',
   templateUrl: './seo.component.html',
   styleUrls: ['./seo.component.scss']
 })
-export class SeoComponent implements OnInit {
+export class SeoComponent implements OnInit, OnDestroy  {
+
+
+  editor: any =  Editor;
+  html: any =  '';
+  toolbar: Toolbar = [
+    ["bold", "underline"]
+  ];
 
   constructor() { }
 
   ngOnInit(): void {
+    this.editor = new Editor();
+  }
+
+  ngOnDestroy(): void {
+    this.editor.destroy();
   }
 
 }
